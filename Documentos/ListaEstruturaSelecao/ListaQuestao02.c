af69b368608df7b2761b91d3ea2e32cf835b41ce
Geovana <maiageovana19@gmail.com>
2026-08-31 16:53:23 -0400

    Refactor salary adjustment output and comments

--- Documentos/ListaEstruturaSelecao/ListaQuestao02.c
+++ Documentos/ListaEstruturaSelecao/ListaQuestao02.c
@@ -5,16 +5,16 @@ int main(){
     float salario_funcionario = 0, salario_reajustado = 0;
 
     //Entrada de dados
-    printf("Digite o salario do funcionario: ");
+    printf("Digite o salario do funcionario: "); //8000
     scanf("%f",&salario_funcionario);
 
     //Processamento
-    salario_reajustado = salario_funcionario + (salario_funcionario * 0.30);
+    salario_reajustado = salario_funcionario + (salario_funcionario * 0.30); //8000 + (8000 * 0,30) = 8000 + 2400 = 10.500
     
     
     //Condicional e saida de dados
-    if(salario_funcionario > 5.000){
-        printf("Salario reajustado: R$ %.2f\n",salario_reajustado);
+    if(salario_funcionario > 5.000){ //10.400 > 5.000
+        printf("Salario reajustado: R$ %.2f\n",salario_reajustado); //R$10,500
     }else{
         printf("Funcionario nao tem direito ao aumento\n");
     }
