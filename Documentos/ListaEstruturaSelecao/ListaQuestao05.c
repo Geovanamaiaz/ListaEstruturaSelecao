4d5f53dd9833ec7b7c8a0290e744311c23af35b8
Geovana <maiageovana19@gmail.com>
2026-08-31 16:17:12 -0400

    Update ListaQuestao05.c

--- Documentos/ListaEstruturaSelecao/ListaQuestao05.c
+++ Documentos/ListaEstruturaSelecao/ListaQuestao05.c
@@ -5,17 +5,17 @@ int main(){
     float salario_funcionario = 0, salario_reajustado = 0;
 
     //Entrada de dados
-    printf("Digite seu salario: ");
+    printf("Digite seu salario: "); //2500
     scanf("%f",&salario_funcionario);
 
-    //Condicional e saida de dados
-    if(salario_funcionario <=300){
+    //Condicional e saida de dados // 2500>300
+    if(salario_funcionario <=300){ 
         salario_reajustado = salario_funcionario + (salario_funcionario * 0.50);
     }else{
-        salario_reajustado = salario_funcionario + (salario_funcionario * 0.30);
+        salario_reajustado = salario_funcionario + (salario_funcionario * 0.30); // 2500 + (2500 * 0.30) = 2500 + 750 = 3250
     }
 
-    printf("Salario reajustado: R$ %.2f\n",salario_reajustado);
+    printf("Salario reajustado: R$ %.2f\n",salario_reajustado); //  R$3250,00
     
     return 0;
 }
