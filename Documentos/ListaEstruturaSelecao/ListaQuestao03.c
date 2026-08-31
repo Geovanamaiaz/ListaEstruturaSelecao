0207c3120c1935154c0300c3ac1a811db062564b
Geovana <maiageovana19@gmail.com>
2026-08-31 16:47:44 -0400

    Add comments to password check logic

--- Documentos/ListaEstruturaSelecao/ListaQuestao03.c
+++ Documentos/ListaEstruturaSelecao/ListaQuestao03.c
@@ -5,12 +5,12 @@ int main(){
     char senha[6];
 
     //Entrada de dados
-    printf("Digite sua senha: ");
+    printf("Digite sua senha: "); //ASDFG
     scanf("%s", &senha);
 
     //Condicional e saida de dados
-    if(senha[0] == 'A' && senha[1] == 'S' && senha[2] == 'D' && senha[3] == 'F' && senha[4] == 'G' && senha[5] == '\0'){
-        printf("Permissao de acesso\n");
+    if(senha[0] == 'A' && senha[1] == 'S' && senha[2] == 'D' && senha[3] == 'F' && senha[4] == 'G' && senha[5] == '\0'){ //ASDFG
+        printf("Permissao de acesso\n"); // Permissão aceita
     }else{
         printf("Acesso negado\n");
     }
